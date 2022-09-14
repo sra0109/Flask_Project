e7c3079f7d783f9d489af7232b354d9c382b654f
Roopa <amruthar0109@gmail.com>
2022-09-14 22:19:03 +0530

    Second day commit

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from email import message
-from flask import Flask , render_template, abort  #import flask 
+from flask import Flask , render_template, abort , jsonify #import flask 
 from model import db
 # from datetime import datetime
 app = Flask(__name__)
 
 @app.route("/") #rootv url and mapped to welcome medthod
 def welcome():
-    return render_template("welcome.html", message = "This is roopa just checking jinja variables and this message comes from view. ")
+    return render_template("welcome.html", cards=db )
 
 
 @app.route("/card/<int:index>")
@@ -14,14 +14,22 @@ def card_view(index):
     try:
 
         card = db[index]
-        return render_template("card.html", card=card)
+        return render_template("card.html", card=card,index=index, max_index = len(db)-1)
     except IndexError:
         abort(404) #if the index is not found
 
 
+@app.route('/api/card/<int:index>')
+def api_card_detail(index):
+    try:
+        return db[index]
+    except IndexError:
+        abort(404)
 
-
-
+@app.route('/api/card')
+def api_card_list():
+    # return db direct return db doesnt work need to use jsonify to return whole list
+    return jsonify(db)
 
 # @app.route("/date")
 # def date():
